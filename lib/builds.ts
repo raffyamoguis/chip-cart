38e0bf7b2c9e00c1fb234b6c3146fc62ffe58544
unknown <raffy.amoguis.projects@gmail.com>
2023-06-12 10:58:33 +0800

    added build dummy data

---
+++ lib/builds.ts
@@ -0,0 +1,89 @@
+import build1 from '../public/builds/a7mw7rasof5b1.webp';
+import build2 from '../public/builds/avkb58zsdd5b1.webp';
+import build3 from '../public/builds/co1tkvdrqf5b1.webp';
+import build4 from '../public/builds/ptq83c9jfe5b1.webp';
+import build5 from '../public/builds/m4xly7oijh5b1.webp';
+import build6 from '../public/builds/9ywl9coyfe5b1.webp';
+import build7 from '../public/builds/yp9ufsx1zf5b1.webp';
+import build8 from '../public/builds/ou974q8gve5b1.webp';
+import build9 from '../public/builds/8sd6z7tf175b1.webp';
+
+
+const cardData = [
+  {
+    name: "GTX 1660 Super",
+    badge: "",
+    image:
+      build1,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, assumenda.",
+    price: "₱35,000",
+  },
+  {
+    name: "GE FORCE RTX PC Build",
+    badge: "",
+    image:
+      build2,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, assumenda.",
+    price: "₱15,000",
+  },
+  {
+    name: "RTX 3050 TI 4GB Build",
+    badge: "On sale",
+    image:
+      build3,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, assumenda.",
+    price: "₱25,000",
+  },
+  {
+    name: "RTX 3060 TI 8GB Build",
+    badge: "",
+    image:
+      build4,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, assumenda.",
+    price: "₱40,000",
+  },
+  {
+    name: "RTX 1060 Build",
+    badge: "On sale",
+    image:
+      build5,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, assumenda.",
+    price: "₱16,000",
+  },
+  {
+    name: "RTX 4080 12GB Build",
+    badge: "",
+    image:
+      build6,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, assumenda.",
+    price: "₱100,000",
+  },
+  {
+    name: "RTX 4070 12GB Build",
+    badge: "",
+    image:
+      build7,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, assumenda.",
+    price: "₱89,000",
+  },
+  {
+    name: "GEFORCE RTX 4070 12GB Build",
+    badge: "",
+    image:
+      build8,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, assumenda.",
+    price: "₱84,000",
+  },
+  {
+    name: "GEFORCE GTX970 4GB Build",
+    badge: "",
+    image:
+      build9,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, assumenda.",
+    price: "₱20,000",
+  }
+];
+
+export function getBuildData() {
+    return cardData;
+}
